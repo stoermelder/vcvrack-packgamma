f390dfe35fcd76256dbc0cb6a085d561eb8f71dd
stoermelder <ben@kristall-klang.de>
2019-10-09 22:12:45 +0200

    panel wip

--- src/DecayMk1.cpp
+++ src/DecayMk1.cpp
@@ -71,10 +71,11 @@ struct DecayMk1Widget : ModuleWidget {
 		addChild(createWidget<MyBlackScrew>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<MyBlackScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addInput(createInputCentered<PJ301MPort>(Vec(22.5f, 128.6f), module, DecayMk1Module::DECAY_INPUT));
-		addParam(createParamCentered<MyTrimpot>(Vec(22.5f, 153.7f), module, DecayMk1Module::DECAY_PARAM));
-		addInput(createInputCentered<PJ301MPort>(Vec(22.5f, 195.2f), module, DecayMk1Module::AMP_INPUT));
-		addParam(createParamCentered<MyTrimpot>(Vec(22.5f, 220.3f), module, DecayMk1Module::AMP_PARAM));
+		addInput(createInputCentered<PJ301MPort>(Vec(22.5f, 83.5f), module, DecayMk1Module::DECAY_INPUT));
+		addParam(createParamCentered<MyTrimpot>(Vec(22.5f, 108.6f), module, DecayMk1Module::DECAY_PARAM));
+		addInput(createInputCentered<PJ301MPort>(Vec(22.5f, 150.0f), module, DecayMk1Module::AMP_INPUT));
+		addParam(createParamCentered<MyTrimpot>(Vec(22.5f, 175.2f), module, DecayMk1Module::AMP_PARAM));
+		
 		addInput(createInputCentered<PJ301MPort>(Vec(22.5f, 280.6f), module, DecayMk1Module::GATE_INPUT));
 		addOutput(createOutputCentered<PJ301MPort>(Vec(22.5f, 323.8f), module, DecayMk1Module::ENV_OUTPUT));
 	}
