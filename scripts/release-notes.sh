ed3fdd828a239a18174d6d05c4de5f2ee33d318c
stoermelder <ben@kristall-klang.de>
2021-06-03 14:41:49 +0200

    update SDK and Azure pipeline

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -5,10 +5,8 @@ cat <<- EOH
 
 This build is automatically generated every time I push my changes. As such it is the latest version of the code and may be unstable, unusable, unsuitable for human consumption, and so on.
 
-The latest stable build can be found at the [VCV Library](https://vcvrack.com/plugins.html#packgamma).
-
 These assets were built against
-https://vcvrack.com/downloads/Rack-SDK-1.1.5.zip
+https://vcvrack.com/downloads/Rack-SDK-1.1.6.zip
 
 The build date and most recent commits are:
 EOH
@@ -16,4 +14,4 @@ date
 echo ""
 echo "Most recent commits:" 
 echo ""
-git log --pretty=oneline | head -3
+git log --pretty=oneline | head -5
